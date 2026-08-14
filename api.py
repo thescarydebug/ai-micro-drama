fb70b13e74c1b7a6b23e1b11129546c4b47f7134
Tahammul Hussain <th22btb0a18@student.nitw.ac.in>
2026-08-14 20:15:36 +0530

    Update api.py

--- api.py
+++ api.py
@@ -29,10 +29,8 @@ app.mount(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
