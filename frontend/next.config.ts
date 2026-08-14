c75a091bdb6ca3e8279ff0ba2e195387d9fb3d8c
Tahammul Hussain <th22btb0a18@student.nitw.ac.in>
2026-08-14 20:51:29 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://ai-micro-drama.onrender.com/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
